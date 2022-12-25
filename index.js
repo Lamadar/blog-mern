fdef76cea42905a55b2e501e2fdadad80bade4a6
Lamadar <vmikasa.2013@gmail.com>
2022-12-26 00:12:37 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,102 @@
+import express from 'express'
+import jwt from 'jsonwebtoken'
+import bcrypt from 'bcrypt'
+import mongoose from 'mongoose'
+import { registerValidator } from './validations/auth.js'
+import { validationResult } from 'express-validator'
+
+const app = express()
+app.use(express.json())
+mongoose.set('strictQuery', true);
+
+import UserModel from './models/user.js'
+
+try{
+    mongoose.connect('mongodb+srv://user:user@cluster0.qsnkwbc.mongodb.net/blog?retryWrites=true&w=majority')
+    console.log('Mongo connected')
+} catch(e){
+    console.log(e)
+}
+app.post('/auth/login', async(req, res) => {
+    try {
+        const user = await UserModel.findOne({
+            email: req.body.email
+        })
+        if (!user){
+            return res.status(404).json({
+                message: 'User not exist'
+            })
+        }
+
+        const isValidPass = await bcrypt.compare(req.body.password, user._doc.passwordHash)
+        if (!isValidPass) {
+            return res.status(400).json({
+                message: 'Incorrect login or password'
+            })
+        }
+        const token = jwt.sign({
+            _id: user._id,
+        }, 
+        'secret123',
+        {
+            expiresIn: '30d'
+        })
+        const { passwordHash, ...userData } = user._doc
+
+        res.json({
+            ...userData,
+            token,
+        })
+
+    } catch (e) {
+        console.log(e)
+        res.status(500).json({ message: 'Login failed'})
+    }
+})
+
+app.post('/auth/register', registerValidator, async(req, res) => {
+
+    try{
+        const errors = validationResult(req)
+        if (!errors.isEmpty()){
+            return res.status(400).json(errors.array())
+        }
+        const password = req.body.password
+        const salt = await bcrypt.genSalt(10);
+        const hash = await bcrypt.hash(password, salt)
+
+        const doc = new UserModel({
+            email: req.body.email,
+            fullName: req.body.fullName,
+            avatarUrl: req.body.avatarUrl,
+            passwordHash: hash
+        })
+
+        const user = await doc.save()
+
+        const token = jwt.sign({
+            _id: user._id,
+        }, 
+        'secret123',
+        {
+            expiresIn: '30d'
+        })
+
+        const { passwordHash, ...userData } = user._doc
+
+        res.json({
+            ...userData,
+            token,
+        })
+    } catch (e) {
+        console.log(e)
+        res.status(409).json({ message: 'Registration failed'})
+    }
+})
+
+app.listen(4444, (err) => {
+    if (err){
+        return console.log(err)
+    }
+    console.log('Server running')
+})
